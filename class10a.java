633fa76ccaba0aa531c59f9573e8e0a3f0e389e9
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-27 01:36:09 +0800

    Add files via upload

---
+++ class10a.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package java09;
+class CRational {// 分數類別
+	int n;
+	int d;
+public void setN(int num) {// 設定分子
+	n=num;
+}
+public void setD(int num) {// 設定分母
+	d=num;
+}
+public void show() { // 顯示分數
+ System.out.println(n+"/"+d);
+}
+public void setND(int num,int den){
+	n=num;
+	d=den;
+	}
+}
+public class class10a{
+	public static void main(String args[]) {
+		CRational aaa=new CRational();
+		CRational bbb=new CRational();
+		aaa.setN(2);
+		aaa.setD(5);
+		aaa.show();
+		bbb.setND(3,7);
+		bbb.show();
+}
+} 
