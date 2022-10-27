2769cea9ee5119d9e3bca37bdecc22309904e87e
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-27 23:00:24 +0800

    Add files via upload

---
+++ class18.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package java09;
+public class class18{
+	public static void main(String args[]){
+ (
+		 new CCircle(1.0)
+{
+	void show(){
+		System.out.println("area="+pi*radius*radius);
+}
+	}
+		 ).show();
+	}
+
+ static class CCircle{
+	 public double pi=3.14;
+	 	public double radius;
+	 	public CCircle(double r){
+	 			radius=r;
+	 }
+}
+	} 
